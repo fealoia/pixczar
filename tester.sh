221fdc593a02981b3e831ec6697be416dd514b25
Matias Lirman <matlirman@gmail.com>
2018-02-20 18:24:44 -0500

    fixed tester script to show colors and added some test cases

--- tester.sh
+++ tester.sh
@@ -1,32 +1,36 @@
 #!/bin/sh
 
 
+
 passing_tests="tests/passing_tests/*"
 failing_tests="tests/failing_tests/*"
 
 success="SUCCESS"
 failure="FAILURE"
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
 
 for file in $passing_tests
 do
-  echo "running $file \n"
+  echo "${reset}running $file \n"
   output=$(./pixczar.native $file)
   if  [ -z "$output" ];
   then
-    echo $failure "\n\n"
+    echo ${red}$failure "\n\n" ${reset}
   else
-    echo $success "\n $output \n"
+    echo ${green}$success "\n ${reset}$output \n"
   fi
 done
 
 for file in $failing_tests
 do
-  echo "running $file \n"
+  echo "${reset}running $file \n"
   output=$(./pixczar.native $file)
   if  [ -z "$output" ];
   then
-    echo $failure "\n\n"
+    echo ${green}$failure "\n\n" ${reset}
   else
-    echo $success "\n $output \n"
+    echo ${red}$success "\n ${reset}$output \n"
   fi
 done
