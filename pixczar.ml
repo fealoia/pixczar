e290311e7f94aee7e0452f25cd3ab90971e112b8
Frank Aloia <fealoia88@gmail.com>
2018-03-27 11:46:55 -0400

    new top level

--- pixczar.ml
+++ pixczar.ml
@@ -1,8 +1,14 @@
-let () =
-    let usage_msg = "usage: ./pixczar.native [file.pxr]" in
-    let channel = ref stdin in
-    Arg.parse [] (fun file -> channel := open_in file) usage_msg;
 let lexbuf = Lexing.from_channel !channel in
-let ast = Parser.program Scanner.token lexbuf in
-let sast = Semant.check ast in
-print_string (Sast.string_of_sprogram sast)
+let ast = Parser.program Scanner.token lexbuf in match !action with
+Ast -> print_string (Ast.string_of_program ast)
+| _ -> let sast = Semant.check ast in (* Semantic checking *)
+match !action with
+Ast -> () (* Included for exhaustiveness *)
+  | Sast    -> print_string (Sast.string_of_sprogram sast)
+    (* Generate LLVM (even if it’s wrong) *)
+  | LLVM_IR -> print_string (Llvm.string_of_llmodule
+                              (Codegen.translate sast))
+    (* Generate and verify LLVM *)
+  | Compile -> let m = Codegen.translate sast in
+  Llvm_analysis.assert_valid_module m; (* useful built-in *) print_string
+  (Llvm.string_of_llmodule m)
