92642506ad101c229535459107213ab59b695ab3
Bryan Li <bryanli.ca@gmail.com>
2018-05-06 22:23:32 -0400

    change relative path for compile.sh

--- opengl/main.c
+++ opengl/main.c
@@ -36,7 +36,7 @@ void color(int rgb[]) {
     float r = (1/255.0)*rgb[1];
     float g = (1/255.0)*rgb[2];
     float b = (1/255.0)*rgb[3];
-    
+
     glColor3f(r, g, b);
 }
 
@@ -53,7 +53,7 @@ void display_rect(int x, int y, int width, int height, int rgb[]) {
 
 void display_triangle(int x, int y, int length, int rgb[]) {
     color(rgb);
-    
+
     glBegin(GL_TRIANGLES);
     glVertex2f(x, y);
     glVertex2f(x + length, y);
@@ -63,12 +63,12 @@ void display_triangle(int x, int y, int length, int rgb[]) {
 
 void display_ellipse(int x, int y, int width, int height, int rgb[]) {
     color(rgb);
-    
+
     const float Pi2=2*3.141593;
     int centerX = x + width/2;
     int centerY = y + height/2;
     int i;
-    
+
     glBegin(GL_TRIANGLE_FAN);
     glVertex2f(centerX, centerY);
     for(i=0; i <= 25; i++) {
@@ -86,15 +86,15 @@ void display_image(int x, int y, int width, int height, char *filename) {
     glBlendFunc (GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
     glEnable(GL_TEXTURE_2D);
     int img_width, img_height;
-    
+
     char absolutepath[1000];
     if(realpath(filename,absolutepath) == NULL) {
         printf("Invalid image: %s\n", filename);
         return;
     }
-    
+
     unsigned char* image =
-    SOIL_load_image(absolutepath, &img_width, &img_height, 0, SOIL_LOAD_RGB);
+    SOIL_load_image(absolutepath, &img_width, &img_height, 0, SOIL_LOAD_RGBA);
 
     if(image == NULL) {
         printf("Invalid image: %s\n", filename);
@@ -104,11 +104,11 @@ void display_image(int x, int y, int width, int height, char *filename) {
     GLuint gltext;
     glGenTextures(1,&gltext);
     glBindTexture(GL_TEXTURE_2D, gltext);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img_width, img_height, 0, GL_RGB,
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, img_width, img_height, 0, GL_RGBA,
                  GL_UNSIGNED_BYTE, image);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    
+
     glBegin(GL_QUADS);
     glTexCoord2i(0, 0); glVertex2i(x, y+height);
     glTexCoord2i(0, 1); glVertex2i(x, y);
@@ -126,26 +126,26 @@ int render(int numFrames, frame *frames[], int fps, int width, int height) {
     glfwInit();
 
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-    
+
     GLFWwindow *window = glfwCreateWindow(width, height, "PixCzar", NULL, NULL);
-    
+
     int screenWidth, screenHeight;
     glfwGetFramebufferSize( window, &screenWidth, &screenHeight);
-    
+
     if( window == NULL ) {
         printf("Failed to create GLFW window\n");
         glfwTerminate();
         return -1;
     }
-    
+
     glfwMakeContextCurrent( window );
     glewExperimental = GL_TRUE;
-    
+
     if( GLEW_OK != glewInit() ) {
         printf("Failed to initialize GLEW\n");
         glfwTerminate();
     }
-    
+
     glViewport( 0, 0, screenWidth, screenHeight );
     glOrtho(0.0f,width, 0.0f, height, -1.0f, 1.0f);
 
@@ -156,10 +156,10 @@ int render(int numFrames, frame *frames[], int fps, int width, int height) {
     int i;
     for(i=1; i<numFrames; i++) {
         if(glfwWindowShouldClose(window)) break;
-        
+
         glClearColor( 1.0f, 1.0f, 1.0f, 1.0f );
         glClear( GL_COLOR_BUFFER_BIT );
-        
+
         placement_node *node = frames[i]->head;
         while(node->placed) {
             if(node->placed->ref->type == 1) {
@@ -175,17 +175,17 @@ int render(int numFrames, frame *frames[], int fps, int width, int height) {
                 display_image(node->placed->x, node->placed->y, node->placed->ref->width,
                               node->placed->ref->height, node->placed->ref->filename);
             }
-            
+
             node = node->next;
         }
         glfwSwapBuffers( window );
-        
+
         lastDrawTime = glfwGetTime();
         glfwPollEvents();
-        
+
         while(glfwGetTime() - lastDrawTime < spf);
     }
-    
+
     glfwTerminate();
     return 0;
 }
